98b21fb700c275654560497b3183c5caa211682e
cescjulbe <cesc.julbe.lopez@gmail.com>
2017-12-08 19:46:00 +0100

    ADDING IMPROVEMENT OT THE QUAL PARSER

--- gp4/src/test/java/gp4tools/gp4/TestQualiParser.java
+++ gp4/src/test/java/gp4tools/gp4/TestQualiParser.java
@@ -22,6 +22,15 @@ public class TestQualiParser extends TestCase {
 			assertEquals("1.27.618", qualiData[0][2]);
 			assertEquals("1", qualiData[0][0]);
 			assertEquals("Lewis Hamilton", qualiData[0][1]);
+			
+			qualiData=qfr.readQualFile("resources/pole1.txt");
+			assertEquals("1.29.236", qualiData[3][2]);
+			assertEquals("12", qualiData[11][0]);
+			assertEquals("Tiago Monteiro", qualiData[15][1]);
+			assertEquals("Pedro De La Rosa", qualiData[17][1]);
+			
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
